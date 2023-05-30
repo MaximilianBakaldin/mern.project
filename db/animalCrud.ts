ba461648d18746fc3b2fc3ae194bb341c2f7c45a
Maximilian3233 <maximilian.bakaldin@iths.se>
2023-05-30 15:40:32 +0200

    Uppdaterat och klart! :)

--- db/animalCrud.ts
+++ db/animalCrud.ts
@@ -6,17 +6,24 @@ export const createAnimal = async (animal: AnimalType) => {
     return newAnimal;
 }
 
-export const getAnimalById = async (id: string) => {
-    const animal = await AnimalModel.findById(id);
-    return animal;
+export const getAllAnimals = async () => {
+    const animals = await AnimalModel.find();
+    return animals;
   }
 
-  export const updateAnimal = async (id: string, animal: AnimalType) => {
-    const updatedAnimal = await AnimalModel.findByIdAndUpdate(id, animal, { new: true });
+  export const getAnimalByName = async (name: string) => {
+    const animal = await AnimalModel.findOne({ name });
+    return animal;
+  };
+
+
+  export const updateAnimalByName = async (name: string, animal: AnimalType) => {
+    const updatedAnimal = await AnimalModel.findOneAndUpdate({ name }, animal, { new: true });
     return updatedAnimal;
-  }
+  };
+
 
   export const deleteAnimal = async (id: string) => {
-    const deletedAnimal = await AnimalModel.findByIdAndRemove(id);
+    const deletedAnimal = await AnimalModel.deleteMany({});
     return deletedAnimal;
   }
--- routes/animal.ts
+++ routes/animal.ts
@@ -1,36 +1,40 @@
 import express, { Request, Response } from 'express';
-import { createAnimal, deleteAnimal, getAnimalById, updateAnimal } from '../db/animalCrud';
+import { createAnimal, deleteAnimal, getAllAnimals, getAnimalByName, updateAnimalByName } from '../db/animalCrud';
 
 const router = express.Router();
-
-router.get('/', (req: Request, res: Response) => {
-    res.send('GET animals');
-})
 
 router.post('/', async (req: Request, res: Response) => {
     const createdAnimal = await createAnimal(req.body);
     res.status(201).json(createdAnimal)
 })
 
-router.get('/:id', async (req: Request, res: Response) => {
-    const animalId = req.params.id;
-    const animal = await getAnimalById(animalId);
+router.get('/', async (req: Request, res: Response) => {
+    const animals = await getAllAnimals();
+    res.json(animals);
+  });
+
+  router.get('/:name', async (req: Request, res: Response) => {
+    const animalName = req.params.name;
+    const animal = await getAnimalByName(animalName);
     if (!animal) {
       return res.status(404).json({ message: 'Animal not found' });
     }
     res.json(animal);
   });
 
-  router.put('/:id', async (req: Request, res: Response) => {
-    const animalId = req.params.id;
-    const updatedAnimal = await updateAnimal(animalId, req.body);
+
+
+  router.put('/:name', async (req: Request, res: Response) => {
+    const animalName = req.params.name;
+    const updatedAnimal = await updateAnimalByName(animalName, req.body);
     if (!updatedAnimal) {
       return res.status(404).json({ message: 'Animal not found' });
     }
     res.json(updatedAnimal);
   });
 
-  router.delete('/:id', async (req: Request, res: Response) => {
+
+  router.delete('/', async (req: Request, res: Response) => {
     const animalId = req.params.id;
     const deletedAnimal = await deleteAnimal(animalId);
     if (!deletedAnimal) {
